a21fda333b6440e1266e97194fcaead8f439fa21
Martiinhz <once11caldas.martin@gmail.com>
2024-04-18 16:33:30 -0500

    Server express

---
+++ src/presentation/server.ts
@@ -0,0 +1,14 @@
+import  express  from "express";
+
+export class Server {   
+    
+    public readonly app = express();
+   
+    constructor() {}
+
+   async start() {
+        this.app.listen(3000, () => {
+            console.log(`Server is running on port ${3000}`);
+        });
+   }
+}
