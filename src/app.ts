c21cb6c3bd6dcd53ebcd624da6177d27b9f60498
Martiinhz <once11caldas.martin@gmail.com>
2024-04-23 09:10:57 -0500

    Estructura de carpetas inicial, creacion de servidor y configuracion de envs

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { envs } from "./config"; // importar el archivo envs de config
 import { Server } from "./presentation/server";
 
 (()=> {
@@ -8,5 +9,7 @@ import { Server } from "./presentation/server";
   // todo: await base de datos
   
   // todo: inicio de nuestro server
-      new Server().start();
+      new Server({
+        port: envs.PORT, // asignamos la variable de entorno en nuestra instancia de servidor.
+      }).start();
   }
