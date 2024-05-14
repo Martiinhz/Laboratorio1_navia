6913dcb599217ab7f7f4d1ddcb701fa784b6a20b
Martiinhz <once11caldas.martin@gmail.com>
2024-05-14 14:26:08 -0500

    conectar base de datos mongoDB

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,9 +1,8 @@
-// importamos las librerias 
-import 'dotenv/config';
-import { get} from 'env-var';
-
-// creamos nuestro enum que manejera nuestra envs
+import "dotenv/config";
+import { get } from "env-var";
 export const envs = {
+  PORT: get("PORT").required().asPortNumber(),
 
-    PORT: get('PORT').required().asPortNumber(),
-}
+  MONGO_URL: get("MONGO_URL").required().asString(), //1
+  MONGO_DB_NAME: get("MONGO_DB_NAME").required().asString(), //2
+};
