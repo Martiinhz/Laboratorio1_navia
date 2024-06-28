753299d0596a04508064fe71ac38cecd15a802fa
Martiinhz <once11caldas.martin@gmail.com>
2024-06-28 16:17:40 -0500

    documentacion de la api y endpoints

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -5,20 +5,56 @@ import { AuthDataSourceImpl, AuthRepositoryImpl } from '../../infraestructure'; 
 export class AuthRoutes {
 
 
-    static get routes(): Router {
-  
-      const router = Router();
+  static get routes(): Router {
 
-      const datasource = new AuthDataSourceImpl(); //6
-      const AuthRepository = new AuthRepositoryImpl(datasource); //7
-      const controller = new AuthController(AuthRepository); // 2 // 8 actulizacion
-  
-        // Definir todas mis rutas especificas
-        router.post('/login', controller.loginUser) // 3
-        router.post('/register', controller.registerUser) // 4
-  
-  
-        return router;
-    }
-  
+    const router = Router();
+
+    const datasource = new AuthDataSourceImpl(); //6
+    const AuthRepository = new AuthRepositoryImpl(datasource); //7
+    const controller = new AuthController(AuthRepository); // 2 // 8 actulizacion
+
+    /**
+
+Post track
+@openapi
+/api/auth/login:
+post:
+tags:
+users
+summary: "Login"
+description: Esta ruta permite logearse
+requestBody:
+content:
+application/json:
+responses:
+'200':
+description: Retorna un mensaje de aprobacion
+*/
+    // Definir todas mis rutas especificas
+    router.post('/login', controller.loginUser) // 3
+    /**
+
+Post track
+@openapi
+/api/auth/register:
+post:
+tags:
+users
+summary: "Register"
+description: Esta ruta permite registrarse
+requestBody:
+content:
+application/json:
+schema:  
+$ref: "#/components/schemas/user"
+responses:
+'200':
+description: Retorna un mensaje de aprobacion
+*/
+    router.post('/register', controller.registerUser) // 4
+
+
+    return router;
   }
+
+}
